cbf74a784130e2e13b61eb70a3f497e28db441cb
Yassine OUKESSOU <youkessou@itrust.fr>
2025-02-26 18:59:55 +0100

    Change version number

--- ASRepCatcher/ASRepCatcher.py
+++ ASRepCatcher/ASRepCatcher.py
@@ -34,7 +34,7 @@ def display_banner():
                             | |                                        
                             |_|                                     
 Author : Yassine OUKESSOU
-Version : 0.6.0
+Version : 0.7.0
                             """)
 
 def is_dc_up(dc):
